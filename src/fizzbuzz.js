28ad56b4d87cb8f7c4399629757c6f6a073bca90
John Twyman <john.twyman@greens.org.au>
2023-04-18 13:26:01 +1000

    Tweak initial setup

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,9 @@
 const fizzBuzz = () => {
-  console.log("FizzBuzz")
   return [1, 2];
 };
 
+const output = fizzBuzz(2);
+console.log(output);
+
+// Do this so jest works
 module.exports = fizzBuzz;
--- tests/fizzbuzz.test.js
+++ tests/fizzbuzz.test.js
@@ -1,7 +1,7 @@
 const fizzBuzz = require("../src/fizzbuzz");
 
 describe("fizzBuzz tests", () => {
-  test("When given a number, N, it returns an array from 1 to the number N", () => {
+  test("When given the number 2, it returns an array from 1 to 2", () => {
 
     // Arrange
     const N = 2;
